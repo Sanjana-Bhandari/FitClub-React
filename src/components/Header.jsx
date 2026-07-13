5a9863a5522ff9f05d8efe0aad9ee14f64489fba
Sanjana-Bhandari <kumarisanjana.work@gmail.com>
2026-07-13 21:12:25 +0530

    Fixed mobile responsive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,68 +1,116 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Header.css'
 import Logo from '../assets/logo.png'
 import Bars from '../assets/bars.png'
 import {Link} from "react-scroll"
 const Header = () => {
-  const mobile = window.innerWidth <= 768 ? true : false
+ const [mobile, setMobile] = useState(window.innerWidth <= 768);
+
+useEffect(() => {
+  const handleResize = () => {
+    setMobile(window.innerWidth <= 768);
+  };
+
+  window.addEventListener("resize", handleResize);
+
+  return () => {
+    window.removeEventListener("resize", handleResize);
+  };
+}, []);
+
+
   const [menuOpened, setMenuOpened] = useState(false)
   return (
    <div className="header">
     <img src={Logo} alt="" className='logo' />
-    {menuOpened === false && mobile === true ? (
-      <div 
-      style={{
-        backgroundColor: 'var(--appColor)',
-        padding: '0.5rem',
-        borderRadius: '5px',
+    {mobile ? (
+  <>
+    {!menuOpened ? (
+      <div
+        style={{
+          backgroundColor: "var(--appColor)",
+          padding: "0.5rem",
+          borderRadius: "5px",
+          cursor: "pointer",
         }}
-        onClick={()=> setMenuOpened(true)}
-        >
-      <img src={Bars} alt="" style={{width: '1.5rem', height: '1.5rem'}} />
-    </div>
+        onClick={() => setMenuOpened(true)}
+      >
+        <img
+          src={Bars}
+          alt=""
+          style={{ width: "1.5rem", height: "1.5rem" }}
+        />
+      </div>
     ) : (
-      <ul className='header-menu'>
-      <li>
-        <Link onClick={()=>setMenuOpened(false)}
-        activeClass='active'
-         to='header'
-        spy={true}
-        smooth={true}
-        > 
-        Home</Link>
+      <ul className="header-menu">
+        <li
+          style={{ alignSelf: "flex-end", cursor: "pointer" }}
+          onClick={() => setMenuOpened(false)}
+        >
+          ✕
         </li>
-       <li>
-        <Link onClick={()=>setMenuOpened(false)}
-         to='programs'
-        spy={true}
-        smooth={true}
-        > 
-        Programs</Link>
+
+        <li>
+          <Link
+            to="home"
+            smooth={true}
+            onClick={() => setMenuOpened(false)}
+          >
+            Home
+          </Link>
         </li>
-       <li>
-        <Link onClick={()=>setMenuOpened(false)}
-         to='reasons'
-        spy={true}
-        smooth={true}
-        > 
-        Why Us</Link>
+
+        <li>
+          <Link
+            to="programs"
+            smooth={true}
+            onClick={() => setMenuOpened(false)}
+          >
+            Programs
+          </Link>
         </li>
-       <li>
-        <Link onClick={()=>setMenuOpened(false)}
-         to='plans'
-        spy={true}
-        smooth={true}
-        > 
-        Plans</Link>
+
+        <li>
+          <Link
+            to="reasons"
+            smooth={true}
+            onClick={() => setMenuOpened(false)}
+          >
+            Why Us
+          </Link>
         </li>
-      <li onClick={()=>setMenuOpened(false)}> <Link
-        onClick={()=>setMenuOpened(false)}
-        to='testimonials'
-        spy={true}
-        smooth={true}
-      >Testimonials</Link></li>
-    </ul>
+
+        <li>
+          <Link
+            to="plans"
+            smooth={true}
+            onClick={() => setMenuOpened(false)}
+          >
+            Plans
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="testimonials"
+            smooth={true}
+            onClick={() => setMenuOpened(false)}
+          >
+            Testimonials
+          </Link>
+        </li>
+      </ul>
     )}
+  </>
+) : (
+  <ul className="header-menu">
+    <li><Link to="home" smooth={true}>Home</Link></li>
+    <li><Link to="programs" smooth={true}>Programs</Link></li>
+    <li><Link to="reasons" smooth={true}>Why Us</Link></li>
+    <li><Link to="plans" smooth={true}>Plans</Link></li>
+    <li><Link to="testimonials" smooth={true}>Testimonials</Link></li>
+  </ul>
+)}
    </div>
   )
 }
