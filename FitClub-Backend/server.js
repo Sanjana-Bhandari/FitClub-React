ec69bf38a87cff3add880a4bf558ad417597cf8c
Sanjana-Bhandari <kumarisanjana.work@gmail.com>
2026-07-19 16:18:52 +0530

    Added membership and newsletter MongoDB integration

--- FitClub-Backend/server.js
+++ FitClub-Backend/server.js
@@ -5,27 +5,30 @@ const connectDB = require("./config/db");
 
 dotenv.config();
 
-
-// Connect MongoDB
-connectDB();
-
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-const userRoutes = require("./routes/userRoutes");
-app.use("/api/users", userRoutes);
+app.use("/api/users", require("./routes/userRoutes"));
 
-// Test Route
 app.get("/", (req, res) => {
-  res.send("🚀 FitClub Backend is Running...");
+  res.send("FitClub Backend Running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); // Pehle MongoDB connect hoga
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
